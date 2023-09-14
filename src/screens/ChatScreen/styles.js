c9d389be064ad8ac886e8e5c93f59ba9060c5aa8
Alex Wong <wonghoming111105@gmail.com>
2023-09-14 01:15:18 -0400

    Add chat list UI template

--- src/screens/ChatScreen/styles.js
+++ src/screens/ChatScreen/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "../../constants";
 
 const styles = StyleSheet.create({
   Wrapper: {
@@ -7,6 +8,24 @@ const styles = StyleSheet.create({
     padding: 40,
     width: "100%",
   },
+  ChatListItemContainer: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: Colors.ui_light_selected_bg,
+    borderBottomWidth: 2,
+    borderBottomColor: 'white',
+  },
+  ProfilePic: {
+    width: 80,
+    height: 80,
+    borderRadius: 100 / 2,
+    margin: 10
+  },
+  ChatListItemDesc: {
+    display: "flex",
+    flexDirection: "column",
+    paddingVertical: 15
+  }
 });
 
 export default styles;
