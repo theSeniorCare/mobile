93049c2c3d957be7bbd601b1d20bfd9980f65f8a
MoonNek0 <wonghoming111105@gmail.com>
2023-09-14 10:22:47 -0400

    Add Chat List UI (#9)
    * add chat screen
    
    * add chat screen
    
    * Add chat list UI template
    
    * remove duplicated file

--- src/screens/ChatScreen/Chat.js
+++ src/screens/ChatScreen/Chat.js
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image, Pressable } from 'react-native'
 import React from 'react'
+import styles from './styles'
+
+const ChatListItem = props => {
+  return (
+    <>
+      <Pressable android_ripple={{ color: 'white', borderless: false }}
+        style={styles.ChatListItemContainer} onPress={() => {
+          // Direct to chat Room
+        }}>
+        <View>
+          <Image style={styles.ProfilePic} source={{ uri: "https://i.pravatar.cc/1000" }} />
+        </View>
+        <View style={styles.ChatListItemDesc}>
+          <Text>{props.items}</Text>
+          <Text style={{ fontStyle: 'italic', paddingTop: 5 }}>Last Chat Here</Text>
+        </View>
+      </Pressable>
+    </>
+  );
+}
 
 const Chat = () => {
   return (
     <View>
-      <Text>ChatScreen</Text>
+      <FlatList
+        data={[
+          { key: 'Devin' },
+          { key: 'Dan' },
+          { key: 'Dominic' },
+          { key: 'Jackson' },
+          { key: 'James' },
+          { key: 'Joel' },
+          { key: 'John' },
+          { key: 'Jillian' },
+          { key: 'Jimmy' },
+          { key: 'Julie' },
+        ]}
+        renderItem={({ item }) =>
+          <ChatListItem items={item.key} />}
+      />
     </View>
   )
 }
