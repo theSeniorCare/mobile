f288d354b2d55ba64e5653b04a20c768755e697a
Maaz <maaz.kom@gmail.com>
2023-09-15 09:03:19 -0400

    landing page done

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -3,12 +3,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signup from '../screens/SignupScreen/Signup';
 import Login from '../screens/LoginScreen/Login';
 import BeforeSignup from '../screens/SignupScreen/BeforeSignup';
+import Landing from '../screens/LandingScreen/Landing';
 
 const Stack = createNativeStackNavigator();
 
 export function AuthStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Landing'>
+      <Stack.Screen name='Landing' component={Landing} />
       <Stack.Screen name='Login' component={Login} />
       <Stack.Screen name='Signup' component={Signup} />
       <Stack.Screen name='BeforeSignup' component={BeforeSignup} />
---
+++ src/screens/LandingScreen/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: 'Figtree_700Bold',
+    color: '#FEEADA',
+    // color: '#839D8E',
+  },
+  subTitle: {
+    fontSize: 16,
+    fontFamily: 'Figtree_500Medium',
+    color: '#FEEADA',
+  },
+  titleGreen: {
+    fontSize: 30,
+    fontFamily: 'Figtree_700Bold',
+
+    color: '#839D8E',
+  },
+  subTitleGreen: {
+    fontSize: 16,
+    fontFamily: 'Figtree_500Medium',
+    color: '#839D8E',
+  },
+  buttonStyle: {
+    color: 'white',
+    margin: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Figtree_500Medium',
+  },
+});
+
+export default styles;
