ea7b5ad53cccfc057792fd17e37964cdc146555c
Muhammad Maaz Kamal <34510708+maz341@users.noreply.github.com>
2023-09-15 11:08:48 -0400

    Splash done (#14)

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -4,17 +4,23 @@ import Signup from '../screens/SignupScreen/Signup';
 import Login from '../screens/LoginScreen/Login';
 import BeforeSignup from '../screens/SignupScreen/BeforeSignup';
 import Landing from '../screens/LandingScreen/Landing';
+import Splash from '../screens/SplashScreen/Splash';
 import { DeveloperOptionStack } from "./DeveloperOptionStack";
 
 const Stack = createNativeStackNavigator();
 
 export function AuthStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Landing'>
-      <Stack.Screen name='Landing' component={Landing} />
-      <Stack.Screen name='Login' component={Login} />
-      <Stack.Screen name='Signup' component={Signup} />
-      <Stack.Screen name='BeforeSignup' component={BeforeSignup} />
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Splash"
+    >
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="Landing" component={Landing} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="BeforeSignup" component={BeforeSignup} />
       <Stack.Screen name="DeveloperOption" component={DeveloperOptionStack} />
+    </Stack.Navigator>
   );
 }
